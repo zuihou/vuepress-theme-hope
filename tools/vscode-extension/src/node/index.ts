961a478463ee2646778dfb971525460f7c0d1200
Mr.Hope <mister-hope@outlook.com>
2022-12-20 22:31:55 +0800

    feat: update

--- tools/vscode-extension/src/node/index.ts
+++ tools/vscode-extension/src/node/index.ts
@@ -1,14 +1,6 @@
-import {
-  attrs,
-  figure,
-  footnote,
-  imageLazyload,
-  // imageMark,
-  imageSize,
-  sub,
-  sup,
-  tasklist,
-} from "vuepress-plugin-md-enhance";
+import { attrs } from "./attrs.js";
+import { figure } from "./figure.js";
+
 import type MarkdownIt from "markdown-it";
 import type { RendererContext } from "vscode-notebook-renderer";
 
@@ -17,23 +9,25 @@ interface MarkdownItRenderer {
 }
 
 export const activate = async (context: RendererContext<void>) => {
+  // FIXME: Debug
+  console.log("Plugin loaded");
+
   const markdownItRenderer = (await context.getRenderer(
     "vscode.markdown-it-renderer"
   )) as MarkdownItRenderer | undefined;
 
-  if (!markdownItRenderer) {
+  if (!markdownItRenderer)
     throw new Error(`Could not load 'vscode.markdown-it-renderer'`);
-  }
 
   markdownItRenderer.extendMarkdownIt((md: MarkdownIt) => {
     md.use(attrs);
     md.use(figure);
-    md.use(footnote);
-    md.use(imageLazyload);
+    // md.use(footnote);
+    // md.use(imageLazyload);
     // md.use(imageMark);
-    md.use(imageSize);
-    md.use(sub);
-    md.use(sup);
-    md.use(tasklist);
+    // md.use(imageSize);
+    // md.use(sub);
+    // md.use(sup);
+    // md.use(tasklist);
   });
 };
